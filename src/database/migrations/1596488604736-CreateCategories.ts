1eaa1866bff8f01255b5e4a791433c5ef17c1937
tmseixas <thiago.ms@gmail.com>
2020-08-03 21:25:47 -0300

    :sparkles: desafio finalizado

---
+++ src/database/migrations/1596488604736-CreateCategories.ts
@@ -0,0 +1,58 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateCategories1596231859524
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    console.log('MIGRATION CATEGORIES');
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+    console.log('MIGRATION FOREIGN');
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionCategory',
+        columnNames: ['category_id'],
+        referencedTableName: 'categories',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+    await queryRunner.dropTable('categories');
+  }
+}
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,34 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = this.find();
+
+    const { income, outcome } = (await transactions).reduce(
+      (acc, trans) => {
+        switch (trans.type) {
+          case 'income':
+            acc.income += Number(trans.value);
+            break;
+
+          case 'outcome':
+            acc.outcome += Number(trans.value);
+            break;
+
+          default:
+            break;
+        }
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
